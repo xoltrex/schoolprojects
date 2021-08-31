3ba2b15f418ce475b07d284bd20dcf4625b16e11
xol <71318437+xoltrex@users.noreply.github.com>
2021-08-31 12:22:33 +0300

    HOPEFULLY last change

--- Dice/index.js
+++ Dice/index.js
@@ -1,5 +1,6 @@
 var score1 = 0;
 var score2 = 0;
+var tempScore = 0;
 var round = 0;
 var turn = "One"
 var doubles = false;
@@ -9,7 +10,7 @@ var doubles2 = 0;
 function rollDice(){
   var result = 0;
   var history = document.getElementById("his");
-  var content = document.createTextNode("Turn: " + turn + " Round: "+ round + " Points: " + result);
+  var content = document.createTextNode("Round: " + round + " Turn: "+ turn + " Points: " + result);
   var lol = document.createElement('br');
   var die1 = document.getElementById("die1");
   var die2 = document.getElementById("die2");
@@ -18,42 +19,47 @@ function rollDice(){
   var diceTotal = d1 + d2;
   die1.innerHTML = d1;
   die2.innerHTML = d2;
-  
+
   if(d1 == d2 && diceTotal !=2){
     document.getElementById("status").innerHTML = "DOUBLES! You get double points!";
       result=result + diceTotal*2;
+      tempScore+result;
       round=round +1;
       doubles=true;
-      addScore(diceTotal *2)
+      addScore(result)
       doubleCounter()
-      content = document.createTextNode("Turn: " + turn + " Round: "+ round + " Points: " + result);
+      content = document.createTextNode("Round: " + round + " Turn: "+ turn + " Points: " + result);
       history.appendChild(content);
       history.appendChild(lol);
   } else if (d1==d2 && diceTotal==2){
     document.getElementById("status").innerHTML = "Double ones! You get 25 points!";
       result=result +25;
+      tempScore+result;
       round=round +1;
       doubles=false;
-      addScore(25)
-      content = document.createTextNode("Turn: " + turn + " Round: "+ round + " Points: " + result);
+      addScore(result)
+      content = document.createTextNode("Round: " + round + " Turn: "+ turn + " Points: " + result);
       history.appendChild(content);
       history.appendChild(lol);
   } else if (d1==1 || d2==1){
     document.getElementById("status").innerHTML = "You got a single 1. No points for you.";
       result=result +0;
+      tempScore=0;
       round=round +1;
       doubles=false;
-      addScore(0)
-      content = document.createTextNode("Turn: " + turn + " Round: "+ round + " Points: " + result);
+      addScore(result)
+      content = document.createTextNode("Round: " + round + " Turn: "+ turn + " Points: " + result);
+      endRound();
       history.appendChild(content);
       history.appendChild(lol);
   } else {
     document.getElementById("status").innerHTML = "You rolled "+diceTotal+".";
       result=result +diceTotal;
+      tempScore+result;
       round=round +1;
       doubles=false;
-      addScore(diceTotal)
-      content = document.createTextNode("Turn: " + turn + " Round: "+ round + " Points: " + result);
+      addScore(result)
+      content = document.createTextNode("Round: " + round + " Turn: "+ turn + " Points: " + result);
       history.appendChild(content);
       history.appendChild(lol);
   }
@@ -61,23 +67,28 @@ function rollDice(){
 
 function addScore(amount) {
   function Counter(){
-    if (round % 2 == 0) {
-      score1 = score1 + amount;
-      turn = "One"
-    } else {
-      score2 = score2 + amount;
-      turn = "Two"
-  } if (doubles===true) {
-      if (round % 2 == 0) { doubles1 = doubles1 +1;
+    tempScore=tempScore + amount;
+    if (doubles===true) {
+      if (turn == "One") { 
+        doubles1 = doubles1 +1;
       } else { doubles2 = doubles2 +1;}
-    } else if (round % 2 == 0) { doubles1 = 0;
-      } else { doubles2 = 0;}
-  }
-  Counter();
-  document.getElementById("score1").innerHTML = score1;
-  document.getElementById("score2").innerHTML = score2;
+    } else if (turn == "One") { 
+      doubles1 = 0;
+    } else { doubles2 = 0;}
+  }Counter();
   document.getElementById("doubles1").innerHTML = doubles1;
   document.getElementById("doubles2").innerHTML = doubles2;
+  document.getElementById("tempScore").innerHTML = tempScore;
+}
+
+function endRound() {
+  if (turn == "One") { turn="Two"; score1 = score1 + tempScore; doubles1=0;
+  } else { turn="One"; score2 = score2 + tempScore; doubles2=0;}
+  tempScore=0;
+  document.getElementById("score1").innerHTML = score1;
+  document.getElementById("score2").innerHTML = score2;
+  document.getElementById("tempScore").innerHTML = tempScore;
+
   if (score1 >= 100) {
     if(alert('Player 1 wins!')){}
     else window.location.reload();
@@ -90,11 +101,11 @@ function addScore(amount) {
 function doubleCounter() {
   if (doubles1 == 3) {
     document.getElementById("status").innerHTML = "Player 1 reached 3 doubles! Points forfeited!";
-    score1 = 0;
+    tempScore = 0;
     doubles1 = 0;
   } else if (doubles2 == 3) {
     document.getElementById("status").innerHTML = "Player 2 reached 3 doubles! Points forfeited!";
-    score2 = 0;
+    tempScore = 0;
     doubles2 = 0;
   }
 }
