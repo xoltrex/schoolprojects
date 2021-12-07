431370d8577adc6e409322136784e7458cb861ea
xoltrex <71318437+xoltrex@users.noreply.github.com>
2021-12-07 14:43:18 +0200

    Update scripts.js

--- Voting/js/scripts.js
+++ Voting/js/scripts.js
@@ -34,21 +34,60 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+const data = [
+  {
+    username: 'ADMIN',
+    password: '1234'
+  }
+]
+
+const response = [
+  {
+    username: document.getElementById('uid').value,
+    password: document.getElementById('psw').value
+  }
+]
+
 var isAdmin = false;
-function logIn() {
-var uname = 'ADMIN';
-var psw = '1234';         //whats opsec
-var username = document.getElementById('uname').value;
-var password = document.getElementById('psw').value;
-  localStorage.setItem('name', username)
-  localStorage.setItem('password', password)
-  if(uname === username || psw === password) {
-  }
+var un = document.getElementById("uid");
+var pw = document.getElementById("psw");
+un.addEventListener("input", function() {response[0].username = document.getElementById('uid').value})
+pw.addEventListener("input", function() {response[0].password = document.getElementById('psw').value})
+async function logIn() {
+  if(response[0].username == data[0].username
+  && response[0].password == data[0].password) {
+    const token = [response[0].username, response[0].password]
+    localStorage.setItem('token', token)
+    console.log("success")
+    isLogged()
+  } else alert("no")
 }
-function checkAdmin() {
-  if(!isAdmin) {
-    document.getElementById('note').style.display = "block";
-  } else {
-    alert("youre no admin")
-  }
+
+function isLogged() {
+  const Token = localStorage.getItem('token')
+  if (!Token) return false; else console.log("lol")
+  return true
 }
+
+async function checkAdmin() {
+  if(isLogged()) {
+    isAdmin = true;
+    if(isAdmin==true){
+      document.getElementById('test').style.display = "block";
+      document.getElementById('ko').style.display = "block";
+      document.getElementById('ok').style.display = "none";
+    } return true;
+  } else return false;
+}
+
+function logOut() {
+  document.getElementById('test').style.display = "none";
+  document.getElementById('ko').style.display = "none";
+  document.getElementById('ok').style.display = "block";
+  localStorage.removeItem('token')
+  isAdmin=false
+  window.location.reload();
+}
+
+window.onload = checkAdmin();
